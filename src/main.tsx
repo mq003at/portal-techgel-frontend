8338114ece3df1da02fbeba74c73b181d8b0fbfa
Quan Nguyen <mq003at@gmail.com>
2025-03-26 06:00:17 +0200

    open msw for deployment testing

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.tsx";
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
+  // if (process.env.NODE_ENV !== "development") {
+  //   return;
+  // }
   const { worker } = await import("./mocks/sharedInstance.tsx");
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
